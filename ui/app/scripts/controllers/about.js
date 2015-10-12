3c5a3cf51d68b0230f5f9f60b30335e5d6477070
Adam Nelson <anelson@apocryph.org>
2015-10-12 11:40:00 -0400

    Create skeleton of Angular app which will render the analyses of the word vectors

---
+++ ui/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name uiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the uiApp
+ */
+angular.module('uiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
